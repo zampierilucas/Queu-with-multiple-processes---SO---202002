751d5f71dea91ea80a02a1b0654abc942066753f
Lucas Zampieri <lcasmz54@gmail.com>
2020-09-06 23:03:47 -0300

    True Initial

---
+++ main.c
@@ -0,0 +1,167 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+
+#define MAX 256
+
+enum frutas {
+  banana,
+  uva,
+  abacate,
+  abacaxi,
+  sizeOf
+} fruta;
+
+struct produto {
+  short int fruta;
+  short int qualidade;
+} produto;
+
+#define READ 0
+
+FILE *esteira_lock(char *operacao, char *file_name) {
+
+  FILE *file = fopen(file_name, operacao);
+
+  while (file == NULL) {
+    printf("File in use, waiting\n");
+    sleep(1);
+    file = fopen(file_name, operacao);
+  }
+
+  return file;
+}
+
+void esteira(char *operacao, struct produto *prod, char *fila) {
+  FILE *file = esteira_lock(operacao, fila);
+
+  if (strcmp(operacao, "a") == 0) {
+    struct produto object;
+
+    object.fruta = (*prod).fruta;
+    object.qualidade = (*prod).qualidade;
+
+    printf("PF: %d Q: %d\n", (*prod).fruta, (*prod).qualidade);
+    printf("OF: %d Q: %d\n", object.fruta, object.qualidade);
+
+    fwrite(&object, sizeof(struct produto), 1, file);
+
+  } else if (strcmp(operacao, "r") == 0) {
+    printf("reading\n");
+
+    // Pega o produto a ser classificado
+    fread(prod, sizeof(struct produto), 1, file);
+    printf("prod: %d quali:%d\n", prod->fruta, prod->qualidade);
+
+    // Remove o produto da fila
+    struct produto object2[256];
+    int i = 0;
+    while (!feof(file)) {
+      fread(&object2[i], sizeof(struct produto), 1, file);
+      printf("obj2: %d quali:%d %d\n", object2[i].fruta, object2[i].qualidade, i);
+      i++;
+      int c = fgetc(file);
+      if (c == EOF) {
+        printf("Hit EOF\n");
+        ungetc(c, file);
+      } else
+        ungetc(c, file);
+    }
+
+    freopen("fila", "w", file);
+    for (int j = 0; j < i; j++) {
+      fwrite(&object2[j], sizeof(struct produto), 1, file);
+    }
+
+    fclose(file);
+    printf("closed\n");
+  }
+}
+
+void classifica_esteira() {
+  // exit(0);
+  char *mode = "r";
+  struct produto prod;
+
+  esteira(mode, &prod, "fila");
+
+  printf("CLASSIFICA: F: %d Q: %d\n", prod.fruta, prod.qualidade);
+}
+
+void adiciona_na_esteira() {
+  // exit(0);
+  struct produto prod;
+  enum frutas size_of_enum = sizeOf;
+  srand(getpid()); // Good enought?
+
+  prod.fruta = (rand() % (int)size_of_enum);
+  prod.qualidade = rand() % 100;
+
+  printf("ADD: F: %d Q: %d\n", prod.fruta, prod.qualidade);
+
+  char *mode = "a";
+  esteira(mode, &prod, "fila");
+}
+
+void init_esteira() {
+  FILE *fptr;
+  fptr = fopen("esteira", "r");
+
+  if (fptr == NULL) {
+    fptr = fopen("esteira", "w");
+  }
+  fclose(fptr);
+
+  fptr = fopen("classificacao", "r");
+
+  if (fptr == NULL) {
+    fptr = fopen("classificacao", "w");
+  }
+  fclose(fptr);
+}
+
+int main() {
+  pid_t c1_pid, c2_pid, c3_pid, c4_pid;
+  int DEBUG = 0;
+  init_esteira();
+
+  // Creates 5 children
+  (c1_pid = fork()) && (c2_pid = fork()) && (c3_pid = fork()) && (c4_pid = fork());
+
+  if (c1_pid == 0) {
+    // printf("1 - Adicionando a esteira um produto\n");
+    // adiciona_na_esteira();
+    DEBUG ? printf("1 - Filho: Meu PID = %d, PPID = %d \n", getpid(), getppid()) : false;
+
+    exit(0);
+  } else if (c2_pid == 0) {
+    // printf("2 - Adicionando a esteira um produto\n");
+    adiciona_na_esteira();
+    // sleep(1);
+    exit(0);
+    DEBUG ? printf("2 - Filho: Meu PID = %d, PPID = %d \n", getpid(), getppid()) : false;
+
+  } else if (c3_pid == 0) {
+    // printf("3 - Adicionando a esteira um produto\n");
+    // adiciona_na_esteira();
+    exit(0);
+
+    DEBUG ? printf("3 - Filho: Meu PID = %d, PPID = %d \n", getpid(), getppid()) : false;
+
+  } else if (c4_pid == 0) {
+    classifica_esteira();
+    exit(0);
+
+    DEBUG ? printf("4 - Filho: Meu PID = %d, PPID = %d \n", getpid(), getppid()) : false;
+
+  } else {
+    sleep(2);
+
+    DEBUG ? printf("Pai: PID = %d, PPID = %d \n", getpid(), getppid()) : false;
+  }
+}
