05731a200969843370ceb5edb665151f20927521
Lucas Zampieri <lcasmz54@gmail.com>
2020-09-07 10:44:45 -0300

    more cleanups and pid status on parent

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -17,12 +18,12 @@ enum frutas {
   sizeOf
 } fruta;
 
-struct produto {
-  short int fruta;
-  short int qualidade;
-} produto;
+struct product {
+  short int fruit;
+  short int quality;
+} product;
 
-FILE *esteira_lock(char *operacao, char *file_name) {
+FILE *queue_lock(char *operacao, char *file_name) {
 
   FILE *file = fopen(file_name, operacao);
 
@@ -35,10 +36,11 @@ FILE *esteira_lock(char *operacao, char *file_name) {
   return file;
 }
 void remove_first_from_queue(FILE *file) {
-  struct produto object2[QUEUE_MAX_SIZE];
   int i = 0;
+  struct product fromQueue[QUEUE_MAX_SIZE];
+
   while (!feof(file)) {
-    fread(&object2[i], sizeof(struct produto), 1, file);
+    fread(&fromQueue[i], sizeof(struct product), 1, file);
     // printf("obj2: %d quali:%d %d\n", object2[i].fruta, object2[i].qualidade, i);
     i++;
 
@@ -52,27 +54,27 @@ void remove_first_from_queue(FILE *file) {
 
   freopen("fila", "w", file);
   for (int j = 0; j < i; j++) {
-    fwrite(&object2[j], sizeof(struct produto), 1, file);
+    fwrite(&fromQueue[j], sizeof(struct product), 1, file);
   }
 }
-void esteira(char *operacao, struct produto *prod, char *fila) {
-  FILE *file = esteira_lock(operacao, fila);
+void queue(char *oper, struct product *prod, char *fila) {
+  FILE *file = queue_lock(oper, fila);
 
-  if (strcmp(operacao, "a") == 0) {
-    struct produto object;
+  if (strcmp(oper, "a") == 0) {
+    struct product toQueue;
 
-    object.fruta = (*prod).fruta;
-    object.qualidade = (*prod).qualidade;
+    toQueue.fruit = (*prod).fruit;
+    toQueue.quality = (*prod).quality;
 
     // printf("PF: %d Q: %d\n", (*prod).fruta, (*prod).qualidade);
     // printf("OF: %d Q: %d\n", object.fruta, object.qualidade);
 
-    fwrite(&object, sizeof(struct produto), 1, file);
+    fwrite(&toQueue, sizeof(struct product), 1, file);
 
-  } else if (strcmp(operacao, "r") == 0) {
+  } else if (strcmp(oper, "r") == 0) {
 
     // Get first product on the queue
-    fread(prod, sizeof(struct produto), 1, file);
+    fread(prod, sizeof(struct product), 1, file);
     // printf("READ PROD: %d quali:%d\n", prod->fruta, prod->qualidade);
 
     // Remove product from queue
@@ -84,65 +86,75 @@ void esteira(char *operacao, struct produto *prod, char *fila) {
 void classifica_esteira() {
   // exit(0);
   char *mode = "r";
-  struct produto prod;
+  struct product prod;
 
-  esteira(mode, &prod, "fila");
+  queue(mode, &prod, "fila");
 
-  printf("CLASSIFICA: F: %d Q: %d\n", prod.fruta, prod.qualidade);
+  printf("SORT: F: %d Q: %d\n", prod.fruit, prod.quality);
 }
 
 void adiciona_na_esteira() {
   // exit(0);
-  struct produto prod;
+  struct product prod;
   enum frutas size_of_enum = sizeOf;
   srand(getpid());
 
   // Randomly generate product data
-  prod.fruta = (rand() % (int)size_of_enum);
-  prod.qualidade = rand() % 100;
+  prod.fruit = (rand() % (int)size_of_enum);
+  prod.quality = rand() % 100;
 
-  printf("ADD: F: %d Q: %d\n", prod.fruta, prod.qualidade);
+  printf("ADD: F: %d Q: %d\n", prod.fruit, prod.quality);
 
   char *mode = "a";
-  esteira(mode, &prod, "fila");
+  queue(mode, &prod, "fila");
 }
 
 int main() {
-  pid_t c1_pid, c2_pid, c3_pid, c4_pid;
+  pid_t c1_pid, c2_pid, c3_pid, c4_pid, c5_pid;
   int DEBUG = 0;
 
   // Creates 5 children
-  (c1_pid = fork()) && (c2_pid = fork()) && (c3_pid = fork()) && (c4_pid = fork());
+  (c1_pid = fork()) && (c2_pid = fork()) && (c3_pid = fork()) && (c4_pid = fork()) && (c5_pid = fork());
 
   if (c1_pid == 0) {
-    // printf("1 - Adicionando a esteira um produto\n");
+    DEBUG ? printf("1C-Feeder: PID = %d, PPID = %d \n", getpid(), getppid()) : false;
     // adiciona_na_esteira();
-    DEBUG ? printf("1 - Filho: Meu PID = %d, PPID = %d \n", getpid(), getppid()) : false;
+    exit(0);
 
-    exit(0);
   } else if (c2_pid == 0) {
-    // printf("2 - Adicionando a esteira um produto\n");
+    DEBUG ? printf("2C-Feeder: PID = %d, PPID = %d \n", getpid(), getppid()) : false;
     adiciona_na_esteira();
-    // sleep(1);
     exit(0);
-    DEBUG ? printf("2 - Filho: Meu PID = %d, PPID = %d \n", getpid(), getppid()) : false;
 
   } else if (c3_pid == 0) {
-    // printf("3 - Adicionando a esteira um produto\n");
+    DEBUG ? printf("3C-Feeder: PID = %d, PPID = %d \n", getpid(), getppid()) : false;
     // adiciona_na_esteira();
     exit(0);
 
-    DEBUG ? printf("3 - Filho: Meu PID = %d, PPID = %d \n", getpid(), getppid()) : false;
-
   } else if (c4_pid == 0) {
+    DEBUG ? printf("4C-Consumer: PID = %d, PPID = %d \n", getpid(), getppid()) : false;
     classifica_esteira();
     exit(0);
 
-    DEBUG ? printf("4 - Filho: Meu PID = %d, PPID = %d \n", getpid(), getppid()) : false;
+  } else if (c5_pid == 0) {
+    DEBUG ? printf("5C-Consumer: PID = %d, PPID = %d \n", getpid(), getppid()) : false;
+    // classifica_esteira();
+    exit(0);
 
   } else {
+    DEBUG ? printf("Pai: PID = %d, PPID = %d \n", getpid(), getppid()) : false;
     sleep(2);
 
-    DEBUG ? printf("Pai: PID = %d, PPID = %d \n", getpid(), getppid()) : false;
+    pid_t pid1 = wait(&c1_pid);
+    pid_t pid2 = wait(&c2_pid);
+    pid_t pid3 = wait(&c3_pid);
+    pid_t pid4 = wait(&c4_pid);
+    pid_t pid5 = wait(&c5_pid);
+
+    printf("\n\n\nPai: Filho (PID = %d) terminou, Status = %d \n", pid1, WEXITSTATUS(c1_pid));
+    printf("Pai: Filho (PID = %d) terminou, Status = %d \n", pid2, WEXITSTATUS(c2_pid));
+    printf("Pai: Filho (PID = %d) terminou, Status = %d \n", pid3, WEXITSTATUS(c3_pid));
+    printf("Pai: Filho (PID = %d) terminou, Status = %d \n", pid4, WEXITSTATUS(c4_pid));
+    printf("Pai: Filho (PID = %d) terminou, Status = %d \n", pid5, WEXITSTATUS(c5_pid));
   }
 }
